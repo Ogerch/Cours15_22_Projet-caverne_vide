e6a4a149147b13310ee1f62b194ee113d8515994
Gavriliouk <e2286717@cmaisonneuve.qc.ca>
2026-04-17 09:44:03 -0400

    Devoir Dash

--- Assets/Scripts/Jeu/DeplacementPersonnage.cs
+++ Assets/Scripts/Jeu/DeplacementPersonnage.cs
@@ -13,14 +13,20 @@ public class DeplacementPersonnage : MonoBehaviour
     public InputAction actionMarche;
     public InputAction actionSaut;
 
+    public InputAction actionDash;
+
     [Header("Déplacement horizontal")]
 
     public float vitesse = 10f;
-    public float inputMarche; 
+    public float inputMarche;
 
     [Header("Saut")]
     public float forceSaut = 5f;
+
+    public float forceDash = 10f;
     public bool inputSaut;
+
+    public bool inputDash;
 
 
 
@@ -28,19 +34,28 @@ public class DeplacementPersonnage : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        sr = GetComponentInChildren<SpriteRenderer>(); 
+        sr = GetComponentInChildren<SpriteRenderer>();
+        anim = GetComponentInChildren<Animator>();
     }
 
-    void OnEnable() {
+    void OnEnable()
+    {
         // Set a activer les écouteurs ou désactiver
         actionMarche.Enable();
         actionSaut.Enable();
+        actionDash.Enable();
     }
 
-    void OnDisable() {
+    void OnDisable()
+    {
         actionMarche.Disable();
         actionSaut.Disable();
+        actionDash.Disable();
     }
+
+
+
+
     // Update is called once per frame
     void Update()
     {
@@ -48,20 +63,81 @@ public class DeplacementPersonnage : MonoBehaviour
         if (actionSaut.WasPressedThisFrame())
         {
             inputSaut = true;
-        } else {
+        }
+        else
+        {
             inputSaut = false;
         }
+
+        if (actionDash.WasPressedThisFrame())
+        {
+            inputDash = true;
+
+             anim.SetTrigger("estDash");
+        }
+        else
+        {
+            inputDash = false;
+        }
+
+        if (inputMarche < 0f)
+        {
+            sr.flipX = true;
+        }
+        else if (inputMarche > 0f)
+        {
+            sr.flipX = false;
+        }
+
+        float vitesseAbsolue = Mathf.Abs(rb.linearVelocityX);
+        anim.SetFloat("vitesse", vitesseAbsolue);
+        
+
+        // if ()
+        // {
+        //     anim.SetTrigger("estDash");
+        // }
+
+        // if (rb.linearVelocity < 0f)
+        // {
+        //     sr.flipX;
+        // }
     }
 
-    void FixedUpdate(){
-        if (inputMarche != 0) {
+    void FixedUpdate()
+    {
+        if (inputMarche != 0)
+        {
             Debug.Log("IM WALKING");
             rb.linearVelocityX = vitesse * inputMarche;
         }
 
-        if (inputSaut == true) {
+        if (inputSaut == true)
+        {
             // rb.AddForce(new Vector2(0,1) * forceSaut);
             rb.AddForce(Vector2.up * forceSaut, ForceMode2D.Impulse);
         }
+
+        if (inputDash == true)
+        {
+
+            if (sr.flipX == true)
+            {
+                rb.AddForce(Vector2.left * forceDash, ForceMode2D.Impulse);
+            }
+            else if (sr.flipX == false)
+            {
+                rb.AddForce(Vector2.right * forceDash, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ennemi")
+        {
+            anim.SetTrigger("estBlesse");
+        }
     }
 }
+    
